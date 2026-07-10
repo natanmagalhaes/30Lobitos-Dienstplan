2064c7932843f92c5f24086771afd121f52f15f6
Natan Magalhães <natanmagalhaes@gmail.com>
2026-07-10 22:57:05 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    __APP_BUILD_TIME__: JSON.stringify(new Date().toISOString())
-  }
-});
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
